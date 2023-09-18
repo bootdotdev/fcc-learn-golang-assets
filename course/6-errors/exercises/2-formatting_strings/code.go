92b0d9ba6f1e9ebd5b40c1e29e7068e92a11530d
Yuliia Davydenko <98152890+juliadavydenko@users.noreply.github.com>
2023-09-18 16:06:50 +0200

    Update code.go
    go error handling

--- course/6-errors/exercises/2-formatting_strings/code.go
+++ course/6-errors/exercises/2-formatting_strings/code.go
@@ -1,7 +1,9 @@
 package main
-
+import fmt
 func getSMSErrorString(cost float64, recipient string) string {
-	// ?
+return fmt.sPrintf("SMS that costs $%.2f to be sent to '%v' can not be sent",
+		  cost,
+		  recipient)
 }
 
 // don't edit below this line
